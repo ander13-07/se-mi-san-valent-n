7c69080a9d7056afd7c5ae3f3e29ca7dcb0e3b70
ander13-07 <158997380+ander13-07@users.noreply.github.com>
2024-02-05 20:09:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "ESOOOO!!";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
